81c2adb386a1ee36fb6384d6ad8609362c783df8
Josh David Miller <josh@joshdmiller.com>
2016-06-09 21:55:53 -0700

    refactor: clean up db connection function

--- src/db.js
+++ src/db.js
@@ -3,22 +3,23 @@ import { MongoClient } from 'mongodb';
 import Logger from './logger';
 
 const log = Logger( 'DB' );
-let connection = null;
-
-const connect = Observable.fromNodeCallback( function ( uri, cb ) {
-  if ( ! connection ) {
-    MongoClient.connect( uri, function ( err, conn ) {
-      connection = conn;
-
-      cb( err, conn );
-    });
-  } else {
-    cb( null, connection );
-  }
-});
 
 export default ({ uri }) => {
   log.debug(`connecting to ${uri}`);
+  let connection = null;
+
+  const connect = Observable.fromNodeCallback( function ( uri, cb ) {
+    if ( ! connection ) {
+      MongoClient.connect( uri, function ( err, conn ) {
+        connection = conn;
+
+        cb( err, conn );
+      });
+    } else {
+      cb( null, connection );
+    }
+  });
+
   return connect( uri );
 };
 
