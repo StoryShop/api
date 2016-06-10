488b775160489793c6fd014f68184c98515219ba
Josh David Miller <josh@joshdmiller.com>
2016-06-09 21:21:49 -0700

    fix: reuse db conn when creating observables

--- src/db.js
+++ src/db.js
@@ -3,7 +3,19 @@ import { MongoClient } from 'mongodb';
 import Logger from './logger';
 
 const log = Logger( 'DB' );
-const connect = Observable.fromNodeCallback( MongoClient.connect, MongoClient );
+let connection = null;
+
+const connect = Observable.fromNodeCallback( function ( uri, cb ) {
+  if ( ! connection ) {
+    MongoClient.connect( uri, function ( err, conn ) {
+      connection = conn;
+
+      cb( err, conn );
+    });
+  } else {
+    cb( null, connection );
+  }
+});
 
 export default ({ uri }) => {
   log.debug(`connecting to ${uri}`);
